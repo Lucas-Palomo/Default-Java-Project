cfd8c5c7c3e2c3f8b8aa0c13fb8c0aba30fee4d7
Lucas Palomo <devlpalomo@gmail.com>
2023-03-07 21:18:47 -0300

    feat: :sparkles: Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.com.google.common.base.Optional
+
+group = "tech.devpalomo"
+version = "0.0.1"
+
+val javaVersion = JavaVersion.VERSION_19
+val kotlinVersion: String = Optional.of(KotlinCompilerVersion.VERSION).or("1.8.10")
+
+java {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+}
+
+
+plugins {
+    application
+    kotlin("jvm") version "1.8.10"
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+
+dependencies {
+    compileOnly(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = javaVersion.ordinal.toString()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.isIncremental = true
+}
+
+application {
+    mainClass.set("Main")
+}
